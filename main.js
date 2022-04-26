2af3ab8545886e3dbcfa00b7a1847a85e156856c
Michele Pozzi <123.mpozzi@gmail.com>
2022-04-27 01:11:41 +0200

    Add comparison slider for images

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const form = document.querySelector('.js-search-form');
 
+form.addEventListener('submit', handleSubmit);
+
+
+
+
+function slide(){
+  let slideValue = document.getElementById("slider").value;
+
+  document.getElementById("imgbox").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+
+  //console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+}
