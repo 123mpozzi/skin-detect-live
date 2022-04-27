41eb88ef15db16d54e89f024bac7f5890b9e2c71
Michele Pozzi <123.mpozzi@gmail.com>
2022-04-27 14:52:22 +0200

    Update layout

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const form = document.querySelector('.js-search-form');
-
-form.addEventListener('submit', handleSubmit);
-
-
-
-
 function slide() {
   let slideValue = document.getElementById("slider").value;
 
   document.getElementById("imgbox").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+}
 
-  //console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+function insertRandom() {
+  const sample_list = [
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Jennifer_Lawrence_TIFF_2%2C_2012.jpg/330px-Jennifer_Lawrence_TIFF_2%2C_2012.jpg',
+    'https://i.imgur.com/S2lYrKD.gif',
+    'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    'https://images.unsplash.com/photo-1526758097130-bab247274f58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  ];
+
+  const chosen = sample_list[Math.floor(Math.random()*sample_list.length)];
+  document.getElementById("name").value = chosen;
 }
