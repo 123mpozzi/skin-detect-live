6ffaf407beb3d241cb289fc14fe48b6cc856c6d9
Michele <123.mpozzi@gmail.com>
2022-05-03 15:09:11 +0200

    Refactor and tidy javascript

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+// Image Slider Behavior
 function slide() {
   let slideValue = document.getElementById("slider").value;
 
   document.getElementById("imgbox").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
 }
+
+
+// Pre-defined Samples
 
 const sample_list = [
   'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Jennifer_Lawrence_TIFF_2%2C_2012.jpg/330px-Jennifer_Lawrence_TIFF_2%2C_2012.jpg',
@@ -21,13 +25,147 @@ const sample_list = [
   'https://live.staticflickr.com/3085/2731117809_5f1ebddca4.jpg'
 ];
 
-function insertRandom() {
-  let chosen = sample_list[Math.floor(Math.random()*sample_list.length)];
-  document.getElementById("name").value = chosen;
+function getRandomSample() {
+  return sample_list[Math.floor(Math.random()*sample_list.length)];
 }
 
-// Used before running python code
-// Therefore this script must be included before pyodide scripts!
+function insertRandom() {
+  document.getElementById("name").value = getRandomSample();
+}
+
 function getSamples() {
   return sample_list;
+}
+
+
+// Utilities
+
+function isValidHttpUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function info(string) {
+  console.log(string)
+  document.getElementById("info").innerText = "STATUS - " + string
+  // TODO: Force element redraw
+}
+
+function getImageURL() { 
+  return document.getElementById("name").value
+};
+
+// Credit to @justadudewhohacks
+// https://github.com/tensorflow/tfjs/issues/604#issuecomment-416135683
+function imageToSquare(img, inputSize) {
+  const dims = img instanceof HTMLImageElement 
+    ? { width: img.naturalWidth, height: img.naturalHeight }
+    : img 
+  const scale = inputSize / Math.max(dims.height, dims.width)
+  const width = scale * dims.width
+  const height = scale * dims.height
+
+  const targetCanvas = document.createElement('canvas')
+  targetCanvas .width = inputSize
+  targetCanvas .height = inputSize
+  targetCanvas.getContext('2d').drawImage(img, 0, 0, width, height)
+
+  return targetCanvas
+}
+
+
+// Pyodide Detectors
+
+// Ask the js worker to init python
+function initPyodide(workerSrc) {
+  let pyodideWorker = new Worker(workerSrc);
+
+  // identify a Promise
+  let id = 0; // ID=0 : init python and pyodide
+
+  pyodideWorker.onmessage = (event) => {
+    const { id, ...data } = event.data;
+
+    if (data.info !== undefined) {
+      info(data.info);
+    }
+    if (data.error !== undefined) {
+      console.log(data.error);
+    }
+    if (data.results !== undefined) {
+      updateSlider(data.results);
+    }
+  };
+
+  // init python and pyodide
+  pyodideWorker.postMessage({
+    ...{},
+    id,
+  });
+
+  return pyodideWorker
+}
+
+// Ask the js worker to skin detect
+function skinDetect(pyodideWorker) {
+  // check if URL is valid
+  //document.getElementById('info').innerText = 'STATUS - Checking URL...'
+  info('Checking URL...')
+  if (!isValidHttpUrl(getImageURL())) {
+    info('Invalid URL. Does it start with https:// ?')
+    //document.getElementById('info').innerText = 'STATUS - Invalid URL. Does it start with https:// ?'
+    return;
+  }
+
+  let id = 1;
+
+  const context = {
+    sample_list: getSamples(),
+    img_url: getImageURL(),
+  };
+
+  // request skin detect
+  pyodideWorker.postMessage({
+    ...context,
+    id,
+  });
+}
+
+// Update img(s), slider, and slider container HTML elements with results of skin detection 
+function updateSlider(results) {
+  const img_data = results[0];
+  const ori_data = results[1];
+  const image_width = results[2];
+  const image_height = results[3];
+
+  const imgbox_el = document.getElementById("imgbox");
+  const imgboxori_el = document.getElementById("imgbox-ori");
+  const slider_el = document.getElementById("slider");
+  const slidercont_el = document.getElementById("slider-container");
+
+  // Update widths
+  imgbox_el.style.width = String(image_width) + 'px';
+  imgboxori_el.style.width = String(image_width) + 'px';
+  slider_el.style.width = String(image_width+50) + 'px';
+  // Update heights
+  imgbox_el.style.height = String(image_height) + 'px';
+  imgboxori_el.style.height = String(image_height) + 'px';
+  slider_el.style.height = String(image_height) + 'px';
+  slidercont_el.style.height = String(image_height+50) + 'px';
+
+  imgbox_el.src = "data:image/png;base64," + img_data;
+  imgboxori_el.src = "data:image/png;base64," + ori_data;
+
+  // init comparison slider transparency
+  const slideValue = document.getElementById("slider").value;
+  imgbox_el.style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+
+  slidercont_el.style.visibility = "visible";
 }
